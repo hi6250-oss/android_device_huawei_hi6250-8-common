0c91a579fd27c2ec811a3826479ad324922d41dc
Nolen Johnson <johnsonnolen@gmail.com>
2018-06-18 23:29:17 -0400

    hi6250: Set Bootanimation to half-res
    * Faster boot, smoother animation
    
    Change-Id: I97f27254474442e3299c7b03ea3180528befa06c

--- hi6250.mk
+++ hi6250.mk
@@ -48,6 +48,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
+# Bootanimation
+TARGET_BOOTANIMATION_HALF_RES := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl
