a761e20942d49a898e4c9ec0e02907acb81e1931
Jan Altensen <info@stricted.net>
2020-05-18 12:28:10 +0200

    hi6250: generate vulkan and opencl symlinks
    Change-Id: I25501e0ffcb86b8e21ee44c648b9f14bbb931b8b

--- Android.mk
+++ Android.mk
@@ -10,6 +10,24 @@ ifneq ($(filter barca berlin prague rhone warsaw, $(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
+
+# EGL symlinks
+EGL_LIBS := libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 hw/vulkan.hi6250.so
+
+EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
+$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 32 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so $@
+
+EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(EGL_LIBS))
+$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL 64 lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so $@
+
 # native_packages hack
 VENDOR_NATIVE_PACKAGES_FIXUP := $(TARGET_OUT_VENDOR)/etc/native_packages.xml
 $(VENDOR_NATIVE_PACKAGES_FIXUP): $(TARGET_OUT_VENDOR)/etc/native_packages.bin
@@ -26,6 +44,6 @@ $(RES_NATIVE_PACKAGES_FIXUP): $(TARGET_OUT_VENDOR)/etc/res_pkgs.bin
 	@echo "Move res_pkgs.bin to res_pkgs.xml"
 	$(hide) mv $(TARGET_OUT_VENDOR)/etc/res_pkgs.bin $@
 
-ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_NATIVE_PACKAGES_FIXUP) $(SYSTEM_NATIVE_PACKAGES_FIXUP) $(RES_NATIVE_PACKAGES_FIXUP)
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_NATIVE_PACKAGES_FIXUP) $(SYSTEM_NATIVE_PACKAGES_FIXUP) $(RES_NATIVE_PACKAGES_FIXUP) $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
 
 endif
