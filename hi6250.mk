a7e8ce757b505f0d6ae10923ef30272712597f38
dianlujitao <dianlujitao@lineageos.org>
2021-05-14 16:56:58 +0200

    hi6250: Build a dummy android.hidl.base@1.0 for Oreo-MR0 blobs
    Signed-off-by: dianlujitao <dianlujitao@lineageos.org>
    Change-Id: Id59369186474fa4db311e4c3dfdf853c9b1450a1

--- hi6250.mk
+++ hi6250.mk
@@ -7,6 +7,11 @@
 ## Common Path
 COMMON_PATH := device/huawei/hi6250-8-common
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor
+
 # Init
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/init.recovery.hi6250.rc:$(TARGET_RECOVERY_OUT)/root/init.recovery.hi6250.rc
