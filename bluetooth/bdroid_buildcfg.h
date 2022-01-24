8313a784a543da0515cb72cd2b72bd377fe8d790
DarkJoker360 <simoespo159@gmail.com>
2022-01-24 18:09:15 +0100

    bluetooth: import bdroid_buildcfg and vndcfg
    Change-Id: Ie1dfe29fcc5da6c61fae299534fb51a289d5615c

---
+++ bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BTM_BYPASS_EXTRA_ACL_SETUP TRUE
+
+#define BLE_INCLUDED TRUE
+#define BLE_VND_INCLUDED TRUE
+
+#endif
