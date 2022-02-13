a7479cec8a82a12402978534a6e829a9a91e9cd3
A2L5E0X1 <alexed25@protonmail.com>
2022-02-13 17:52:29 +0100

    hi6250: switch to new 1.2 nfc service
    * fully binderized service for nxp nfc
    * allows up to drop the 15.1 PT hal
    * using 1.0 nfc hal is deprecated and doesn't let us pass the build anymore...
    
    Change-Id: I9ce596b258baab2cd7b3923e574a40da1466f886

--- hi6250.mk
+++ hi6250.mk
@@ -156,11 +156,9 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     libnfc-nci.conf \
-    nfc_nci.nqx.default \
     NfcNci \
     Tag
 
