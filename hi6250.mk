082ee8b650fdb9b4bab3f7093ba25eb063ba7925
A2L5E0X1 <alexed25@protonmail.com>
2022-03-10 13:26:47 +0000

    hi6250: build android.hardware.camera.common@1.0
    * doesn't get built automatically for some reason, but it's needed by camera.provider impl
    
    Change-Id: I55b54b94a2698d83fb630da78a4849ab9844aba2

--- hi6250.mk
+++ hi6250.mk
@@ -53,6 +53,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     Snap
