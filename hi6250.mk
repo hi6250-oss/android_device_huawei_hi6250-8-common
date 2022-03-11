86aa187384ece74d16bc5f14fc627359968caeba
A2L5E0X1 <alexed25@protonmail.com>
2022-03-11 12:05:04 +0000

    hi6250: build camera.provider service
    * emui 8 still has a camera.hi6250 hal, meanwhile emui 9 doesn't but the camera app shows up there, even without camera.provider impl..
    
    Change-Id: If393e0012587e2f6031581fe9af41bbb04518eeb

--- hi6250.mk
+++ hi6250.mk
@@ -53,9 +53,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.common@1.0 \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-service \
     Snap
 
 # Debugging
