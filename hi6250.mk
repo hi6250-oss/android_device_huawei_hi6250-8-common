3bb9334865bc8c801c2586a751b83d87f23f24dd
A2L5E0X1 <alexed25@protonmail.com>
2022-04-15 16:53:34 +0200

    hi6250: import fstab and recovery init
    * fstab taken from WAS-LX1 8.0.0.397(C432)
    
    Change-Id: I94c7731d4e7e0a6dde30c52e96fc58c3336e5371

--- hi6250.mk
+++ hi6250.mk
@@ -6,3 +6,7 @@
 
 ## Common Path
 COMMON_PATH := device/huawei/hi6250-8-common
+
+# Init
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/init/init.recovery.hi6250.rc:$(TARGET_RECOVERY_OUT)/root/init.recovery.hi6250.rc
